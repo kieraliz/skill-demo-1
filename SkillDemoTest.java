37f44d5db614ce508303a716965a39ca86187a6c
kieraliz <kenavarro@ucsd.edu>
2022-04-28 22:29:55 -0700

    Update SkillDemoTest.java

--- SkillDemoTest.java
+++ SkillDemoTest.java
@@ -5,8 +5,10 @@ import org.junit.*;
 public class SkillDemoTest {
     @Test
     public void addition() {
-        assertEquals(2, SkillDemo.add(1, 2));
+        assertEquals(3, SkillDemo.add(1, 2));
     }
 }
 
 // test should fail initially: 1+2 = 3, not 2
+
+// test should pass: 1+2 = 3 and 3=3
